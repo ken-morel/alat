8d2777ac39f400e546a0cfc64c4866e0e2c37062
Engon Ken Morel <engonken8@gmail.com>
2025-08-12 23:17:19 +0100

    added pug, sass

--- apps/desktop/frontend/src/routes/+page.svelte
+++ apps/desktop/frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<template lang="pug">
+  main h1(class="main-heading") Hello from Pug!
+  p This component is styled with
+  SASS.
+</template>
+
+<style lang="sass">
+
+</style>
--- apps/desktop/frontend/svelte.config.js
+++ apps/desktop/frontend/svelte.config.js
@@ -1,12 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
+	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	preprocess: [
+		vitePreprocess(),
+		sveltePreprocess({
+			sass: {
+				// Use indented syntax and prepend global styles
+				indentedSyntax: true,
+				prependData: `@import 'src/lib/styles/variables.sass'`
+			},
+			pug: true
+		})
+	],
+
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
