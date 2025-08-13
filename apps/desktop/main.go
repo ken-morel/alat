9a74a9f1547f869b00ff85dcea726fd51159686c
Engon Ken Morel <engonken8@gmail.com>
2025-08-13 09:41:26 +0100

    altered desktop's structure

--- apps/desktop/main.go
+++ apps/desktop/main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
+	"alat/apps/desktop/app"
 	"embed"
-
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/build
 var assets embed.FS
 
 func main() {
-	app := NewApp()
+	app := app.NewApp(assets)
 
-	err := wails.Run(&options.App{
-		Title:  "desktop",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
-	if err != nil {
+	if err := app.Run(); err != nil {
 		println("Error:", err.Error())
 	}
 }
