c89c84d1ff4821662d7aeb4d86dbcd14879b41b5
Engon Ken Morel <engonken8@gmail.com>
2025-08-21 13:21:03 +0100

    improved theme

--- apps/desktop/frontend/src/routes/+layout.svelte
+++ apps/desktop/frontend/src/routes/+layout.svelte
@@ -3,10 +3,32 @@
   import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
+
+  import { Switch } from "@skeletonlabs/skeleton-svelte";
+
+  let checked = $state(false);
+
+  $effect(() => {
+    const mode = localStorage.getItem("mode") || "light";
+    checked = mode === "dark";
+  });
+
+  const onCheckedChange = (event: { checked: boolean }) => {
+    const mode = event.checked ? "nosh" : "alat";
+    document.documentElement.setAttribute("data-theme", mode);
+    localStorage.setItem("mode", mode);
+    checked = event.checked;
+  };
 </script>
+
+<Switch {checked} {onCheckedChange}></Switch>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <script>
+    const mode = localStorage.getItem("mode") || "light";
+    document.documentElement.setAttribute("data-theme", mode);
+  </script>
 </svelte:head>
 
 {@render children?.()}
