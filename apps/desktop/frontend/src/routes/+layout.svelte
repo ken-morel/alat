113b275e9c0ebca068c708273d22304ea22d9ba7
Engon Ken Morel <engonken8@gmail.com>
2025-08-31 14:17:00 +0100

    added toaster

--- apps/desktop/frontend/src/routes/+layout.svelte
+++ apps/desktop/frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "../app.css";
   import favicon from "$lib/assets/logo.svg";
+  import Toaster from "$lib/Toaster.svelte";
 
   let { children } = $props();
 </script>
@@ -8,5 +9,7 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+
+<Toaster />
 
 {@render children?.()}
