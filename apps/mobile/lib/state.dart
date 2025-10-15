fe0b4693f27835142f68befebc43ac76589f9909
ken-morel <engonken8@gmail.com>
2025-10-15 22:03:52 +0100

    Changed config creation path to allow for libalat client customization

--- apps/mobile/lib/state.dart
+++ apps/mobile/lib/state.dart
@@ -6,12 +6,12 @@ import 'package:path_provider/path_provider.dart';
 
 class AppState extends ChangeNotifier {
   dalat.AlatInstance? _alatInstance;
-  dalat.AppSettings? _appSettings;
-  dalat.ServiceSettings? _serviceSettings;
+  dalat.AppConfig? _appSettings;
+  dalat.ServiceConfig? _serviceSettings;
 
   dalat.AlatInstance? get node => _alatInstance;
-  dalat.AppSettings? get settings => _appSettings;
-  dalat.ServiceSettings? get serviceSettings => _serviceSettings;
+  dalat.AppConfig? get settings => _appSettings;
+  dalat.ServiceConfig? get serviceSettings => _serviceSettings;
 
   bool get isReady => _alatInstance != null && _appSettings != null;
 
@@ -23,6 +23,32 @@ class AppState extends ChangeNotifier {
     }
   }
 
+  static dalat.AppConfig createAppConfig() {
+    final config = dalat.AlatInstance.createAppConfig();
+    config.deviceType = dalat.deviceTypeMobile;
+    if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+      config.deviceType = dalat.deviceTypeDesktop;
+    }
+    return config;
+  }
+
+  static Future<dalat.ServiceConfig> createServiceConfig() async {
+    final config = dalat.AlatInstance.createServiceConfig();
+    config.fileSend.saveFolder =
+        ((await getDownloadsDirectory()) ?? Directory(".")).path;
+    return config;
+  }
+
+  static Future<dalat.AlatInstance> createInstance(String configPath) async {
+    final appConfig = AppState.createAppConfig();
+    final serviceConfig = await AppState.createServiceConfig();
+    return dalat.AlatInstance.create(
+      configPath: configPath,
+      serviceConfig: serviceConfig,
+      appConfig: appConfig,
+    );
+  }
+
   Future<bool> initialize() async {
     final instances = dalat.AlatInstance.getInstances();
     if (instances.isEmpty) {
@@ -31,17 +57,14 @@ class AppState extends ChangeNotifier {
         await configDir.create(recursive: true);
       }
 
-      _alatInstance = dalat.AlatInstance.create(
-        configPath: configDir.path,
-        deviceType: dalat.deviceTypeMobile,
-      );
+      _alatInstance = await AppState.createInstance(configDir.path);
     } else {
       _alatInstance = dalat.AlatInstance.get(instances[0]);
     }
 
-    _appSettings = await _alatInstance!.getAppSettings();
+    _appSettings = await _alatInstance!.getAppConfig();
     if (_appSettings!.setupComplete) _alatInstance!.start();
-    _serviceSettings = await _alatInstance!.getServiceSettings();
+    _serviceSettings = await _alatInstance!.getServiceConfig();
     _setupServices();
 
     // Notify listeners that initialization is complete.
@@ -61,14 +84,14 @@ class AppState extends ChangeNotifier {
       throw "Cannot complete setup, setings or instance not set";
     }
     _appSettings!.setupComplete = true;
-    await _alatInstance!.setAppSettings(_appSettings!);
+    await _alatInstance!.setAppConfig(_appSettings!);
     _alatInstance!.start();
     notifyListeners();
   }
 
   Future<void> saveSettings() async {
-    await _alatInstance!.setAppSettings(_appSettings!);
-    await _alatInstance!.setServiceSettings(_serviceSettings!);
+    await _alatInstance!.setAppConfig(_appSettings!);
+    await _alatInstance!.setServiceConfig(_serviceSettings!);
   }
 
   @override
