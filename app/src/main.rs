a620c00677dc1bca12df5af9a1b8488eff38888e
Engon Ken Morel <engonken8@gmail.com>
2026-02-13 23:23:30 +0100

    feat(app): Created slint template
    Created template slint application, still to receiv content

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use slint::{SharedString, Weak};
+
+mod ui;
+
+use slint::ComponentHandle;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let window = ui::MainWindow::new()?;
+
+    window.run()?;
+
+    Ok(())
 }
